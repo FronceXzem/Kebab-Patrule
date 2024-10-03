44047bdbcee40919734df173a7c2fb6834a5ff7e
Nikita <felasso3212@gmail.com>
2024-10-03 11:09:41 +0300

    contact server

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,12 +2,13 @@ const indexRouter = require("express").Router();
 
 
 const authRouter = require("./auth.routes");
+const dishRouter = require("./dish.routes");
 
 const tokensRouter = require("./tokens.routes");
 
 
 indexRouter.use("/auth", authRouter);
-
+indexRouter.use("/dishes", dishRouter);
 indexRouter.use("/tokens", tokensRouter);
 
 module.exports = indexRouter;
